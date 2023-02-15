0745217d332ec92a377aa074c15248189ff26815
Bekzod <mirzaahmedov.dev@proton.me>
2023-02-15 17:25:58 +0500

    attempt

--- s01.go
+++ s01.go
@@ -1,5 +1,45 @@
 package structs
 
+type User struct {
+	firstName string
+	lastName string
+}
+
+func New(firstName, lastName string) *User {
+	return &User{
+		firstName: firstName,
+		lastName: lastName,
+	}
+}
+func ResetUser(user *User) {
+	user.firstName = ""
+	user.lastName = ""
+}
+func IsUser(value interface{}) bool {
+	switch value.(type) {
+	case User:
+		return true
+	default:
+		return false
+	}
+}
+func ProccessUser(user UserInterface) string {
+	user.SetFirstName("John")
+	user.SetLastName("Doe")
+
+	return user.FullName()
+}
+
+func (u *User)SetFirstName(value string) {
+	u.firstName = value
+}
+func (u *User)SetLastName(value string) {
+	u.lastName = value
+}
+func (u *User)FullName() string {
+	return u.lastName + " " + u.firstName
+}
+
 type UserInterface interface {
 	SetFirstName(string)
 	SetLastName(string)
