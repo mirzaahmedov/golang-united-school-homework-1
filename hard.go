2f017237935bfbb0e43dbb29ee4f107457fa7cd5
Bekzod <mirzaahmedov.dev@gmail.com>
2023-02-28 21:42:53 +0500

    attempt 2

---
+++ hard.go
@@ -0,0 +1,32 @@
+package homework
+
+import (
+  "strings"
+  "fmt"
+  "io"
+)
+
+/*
+You own a Goal Parser that can interpret a string command.
+The command consists of an alphabet of "G", "()" and/or "(al)" in some order.
+The Goal Parser will interpret
+"G" as the string "G", "()" as the string "o", and "(al)" as the string "al".
+The interpreted strings are then concatenated in the original order.
+For example:
+input: "G()(al)
+output: Goal
+input: G()()()()(al)
+output: Gooooal
+input: (al)G(al)()()G
+output: alGalooG
+*/
+func GoalParsers(strReader *strings.Reader) string {
+  str, err := io.ReadAll(strReader)
+  if err != nil {
+    return ""
+  }
+  res := strings.ReplaceAll(string(str), "()", "o")
+  res = strings.ReplaceAll(res, "(al)", "al")
+
+  return res
+}
